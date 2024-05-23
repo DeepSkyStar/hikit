eb984a4610e1b9d8227fe5687f6fa6fa091f1394
deepskystar <deepskystar@outlook.com>
2024-05-23 19:15:28 +0800

    [Fix] fix format error.

--- hi_installer.py
+++ hi_installer.py
@@ -4,7 +4,7 @@
 Author: Cosmade
 Date: 2024-04-09 15:55:33
 LastEditors: deepskystar deepskystar@outlook.com
-LastEditTime: 2024-05-23 18:03:55
+LastEditTime: 2024-05-23 19:14:13
 FilePath: /hikit/hi_installer.py
 Description: 
 
@@ -153,7 +153,8 @@ class HiInstaller(object):
             source_path = os.path.join(path, command)
             bin_content = bin_template.replace("<BIN_PATH>", source_path)
             if venv_path:
-                bin_content = bin_content.replace("<VENV_BEGIN>", f"source {venv_path + "/bin/activate"}")
+                activate_path = venv_path + "/bin/activate"
+                bin_content = bin_content.replace("<VENV_BEGIN>", "source " + activate_path)
                 bin_content = bin_content.replace("<VENV_END>", "deactivate")
             else:
                 bin_content = bin_content.replace("<VENV_BEGIN>", "")
